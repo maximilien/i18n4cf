9c5821bd6988610bb09ab37c8b10015b3b91383b
dr.max <maximilien@acm.org>
2014-09-03 15:11:15 -0700

    added some failing tests

---
+++ integration/modify_string/modify_string_suite_test.go
@@ -0,0 +1,22 @@
+package modify_string_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/maximilien/i18n4go/integration/test_helpers"
+	"github.com/onsi/gomega/gexec"
+
+	"testing"
+)
+
+func TestModifyString(t *testing.T) {
+	BeforeSuite(test_helpers.BuildExecutable)
+
+	AfterSuite(func() {
+		gexec.CleanupBuildArtifacts()
+	})
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ModifyString Suite")
+}
